17d1aa4f67ab588e1dcf158e474d27fe668d830c
Louis Ferdinand Trinkle <louis-ferdinand.trinkle@ufz.de>
2023-11-06 16:04:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, url_for
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired
-from wtforms import StringField, ValidationError
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms.validators import ValidationError
+from wtforms import StringField
 from werkzeug.utils import secure_filename
+from werkzeug.datastructures import FileStorage
 import csv
 from flask_bootstrap import Bootstrap5
 from transformation import process_csv_file
@@ -22,63 +24,61 @@ if not os.path.exists(app.config["UPLOAD_FOLDER"]):
 if not os.path.exists(app.config["DOWNLOAD_FOLDER"]):
     os.makedirs(app.config["DOWNLOAD_FOLDER"])
 
+class UploadForm(FlaskForm):
+    file = FileField(validators=[FileRequired(),
+                                 FileAllowed(['csv'], 'Only CSV allowed')])
+    message = StringField("Chat")
 
-class CsvValidator(object):
-    """
-    Validates that a file uploaded via a Flask-WTF FileField is a CSV file with a specified number of columns.
-
-    Args:
-        num_columns (int): The expected number of columns in the CSV file.
-
-    Raises:
-        ValidationError: If the uploaded file is not a CSV file or does not have the expected number of columns.
-    """
-
-    def __init__(self, num_columns):
-        self.num_columns = num_columns
-
-    def __call__(self, form, field):
-        if not field.data:
-            raise ValidationError("No file uploaded")
-
-        filename = secure_filename(field.data.filename)
-        if not filename.endswith(".csv"):
-            raise ValidationError("File must be a CSV")
+    def validate_file(self, field):
+        if not (isinstance(field.data, FileStorage) and field.data):
+            return
 
         stream = field.data.stream
         stream.seek(0)  # Move the stream cursor to the beginning of the file
-        try:
-            reader = csv.reader(stream.read().decode("utf-8").splitlines())
-            for row in reader:
-                if len(row) != self.num_columns:
-                    raise ValidationError(f"CSV must have {self.num_columns} columns")
-        except csv.Error:
-            raise ValidationError("File is corrupted")
+        reader = csv.reader(stream.read().decode("utf-8").splitlines())
+        for row in reader:
+            if len(row) != 8:
+                raise ValidationError("CSV must have 8 columns")
 
 
-class UploadForm(FlaskForm):
-    file = FileField(validators=[FileRequired(), CsvValidator(8)])
-    message = StringField("Chat")
-
-
-@app.route("/", methods=["GET", "POST"])
-def index():
-    """
-    Renders the index.html template and handles file uploads.
-
-    Returns:
-        If the form is submitted and valid, returns a success message with the file path.
-        Otherwise, returns the rendered index.html template with an UploadForm instance and form errors.
-    """
-    # TODO fix the csv upload
+@app.route('/', methods=['GET', 'POST'])
+def upload():
     form = UploadForm()
+
     if form.validate_on_submit():
-        file = form.file.data
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-        return redirect(url_for('index', filename=filename))
-        return f"File uploaded successfully! Path: {file_path}"
-    return render_template("index.html", form=form)
+        f = form.file.data
+        filename = secure_filename(f.filename)
+        file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+        f.save(file_path)
+
+        osm_data = process_csv_file(file_path)
+        osm_file_path = os.path.join(app.config["DOWNLOAD_FOLDER"], "osm_data.geojson")
+        with open(osm_file_path, "w") as osm_file:
+            geojson.dump(osm_data, osm_file)
+
+    
+    return render_template('index.html', form=form)
+
+
+# @app.route("/", methods=["GET", "POST"])
+# def index():
+#     form = UploadForm()
+#     if form.validate_on_submit():
+#         filename = secure_filename(form.file.data.filename)
+#         file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+#         form.file.data.save(file_path)
+#         # file.save(file_path)
+
+#         osm_data = process_csv_file(file_path)
+#         osm_file_path = os.path.join(app.config["DOWNLOAD_FOLDER"], "osm_data.geojson")
+#         with open(osm_file_path, "w") as osm_file:
+#             geojson.dump(osm_data, osm_file)
+
+#         # Provide a download link for the transformed OSM data
+#         download_link = url_for("download_file", filename="osm_data.geojson")
+
+#         return render_template("index.html", form=form, download_link=download_link)
+#     return render_template("index.html", form=form)
 
 
 if __name__ == "__main__":
