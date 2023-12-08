de5f44a609cc51a96701b852f137dc37df7cf3f2
Louis Ferdinand Trinkle <louis-ferdinand.trinkle@ufz.de>
2023-12-08 17:00:21 +0100

    Add Leaflet Map

--- app.py
+++ app.py
@@ -1,85 +1,247 @@
 import os
-from flask import Flask, render_template, request, url_for
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms.validators import ValidationError
-from wtforms import StringField
+import dash
+from dash import dcc, html, callback_context
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
 from werkzeug.utils import secure_filename
-from werkzeug.datastructures import FileStorage
+import base64
 import csv
-from flask_bootstrap import Bootstrap5
-from transformation import process_csv_file
 import geojson
+from transformation import process_csv_file, transfrom_csv
+from flask import Flask, send_from_directory
+from urllib.parse import quote as urlquote
+import time
+import json
+import pandas as pd
+import geopandas as gpd
+import shapely.geometry
+import numpy as np
+import dash_leaflet as dl
+import random
+from dash_leaflet import express as dlx
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = "secret_key"
-app.config["UPLOAD_FOLDER"] = "upload"
-app.config["DOWNLOAD_FOLDER"] = "download"
+UPLOAD_FOLDER = "upload"
+DOWNLOAD_FOLDER = "download"
 
-bootstrap = Bootstrap5(app)
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
-if not os.path.exists(app.config["UPLOAD_FOLDER"]):
-    os.makedirs(app.config["UPLOAD_FOLDER"])
+if not os.path.exists(DOWNLOAD_FOLDER):
+    os.makedirs(DOWNLOAD_FOLDER)
 
-if not os.path.exists(app.config["DOWNLOAD_FOLDER"]):
-    os.makedirs(app.config["DOWNLOAD_FOLDER"])
+# Mapbox token
+# mb_token = "pk.eyJ1IjoibG91aXN0cmkiLCJhIjoiY2xvbjF4M2h3MDlvZjJ2cXNldThlaG8xdiJ9.mU8urLdPCQo-DyiYhwqtLQ"
 
-class UploadForm(FlaskForm):
-    file = FileField(validators=[FileRequired(),
-                                 FileAllowed(['csv'], 'Only CSV allowed')])
-    message = StringField("Chat")
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-    def validate_file(self, field):
-        if not (isinstance(field.data, FileStorage) and field.data):
-            return
+server = Flask(__name__)
+app = dash.Dash(external_stylesheets=external_stylesheets, server=server)
 
-        stream = field.data.stream
-        stream.seek(0)  # Move the stream cursor to the beginning of the file
-        reader = csv.reader(stream.read().decode("utf-8").splitlines())
-        for row in reader:
+@server.route("/download/<path:path>")
+def download(path):
+    """Serve a file from the upload directory."""
+    return send_from_directory(UPLOAD_FOLDER, path, as_attachment=True)
+
+with open("upload_data/Class3.geojson") as f:
+    Class3 = json.load(f)
+
+with open("upload_data/Class4.geojson") as f:
+    Class4 = json.load(f)
+
+app.layout = html.Div([
+    html.H1("Upload, Transformation, OSM-Request and Display of CSV-data"),
+    html.H2("Upload"),
+    dcc.Upload(
+        id='upload-data',
+        accept=".csv",
+        children=html.Div(
+            ["Drag and drop or click to select a file to upload."]
+        ),
+        multiple=False
+    ),
+    html.H2("File List"),
+    html.Ul(id="file-list"),
+    html.Div(id='output-data-upload'),
+    html.Div(id='output-osm-query'),
+    html.Div(id='file-path', style={'display': 'none'}),
+    html.Div(id='osm-file-path', style={'display': 'none'}),
+    dl.Map([
+        dl.LayersControl(
+            [dl.BaseLayer(dl.TileLayer(), name="OpenStreetMap", checked=True),
+            dl.Overlay(dl.LayerGroup(), name="points", checked=False, id='points'),
+            dl.Overlay(dl.LayerGroup(), name="markers", checked=False, id='markers'),
+            # add the class3 & 4 geojson layers to the layer control
+            dl.Overlay(dl.LayerGroup(dl.GeoJSON(data=Class3, style={'color': 'red'})), name="Class3", checked=True, id='class3'),
+            dl.Overlay(dl.LayerGroup(dl.GeoJSON(data=Class4)), name="Class4", checked=True, id='class4'),
+            ], id="lc"
+        ),
+        dl.FullScreenControl(),
+        dl.LocateControl(locateOptions={'enableHighAccuracy': True}),
+        dl.ScaleControl(position="bottomleft"),
+    ], center=[52.5, 13.4], zoom=8, style={'height': '50vh'}, id='map'),
+    html.Button("Generate markers", id="btn")
+])
+
+def uploaded_files():
+    """List the files in the upload directory."""
+    files = []
+    for filename in os.listdir(UPLOAD_FOLDER):
+        path = os.path.join(UPLOAD_FOLDER, filename)
+        if os.path.isfile(path):
+            files.append(filename)
+    return files
+
+
+def file_download_link(filename):
+    """Create a Plotly Dash 'A' element that downloads a file from the app."""
+    location = "/download/{}".format(urlquote(filename))
+    return html.A(filename, href=location)
+
+
+@app.callback(
+    Output("file-list", "children"),
+    [Input("upload-data", "filename"), Input("upload-data", "contents")],
+)
+def update_output(uploaded_filenames, uploaded_file_contents):
+    """Save uploaded files and regenerate the file list."""
+    time.sleep(1)
+    files = uploaded_files()
+    if len(files) == 0:
+        return [html.Li("No files yet!")]
+    else:
+        return [html.Li(file_download_link(filename)) for filename in files]
+
+# Upload the file and check if it is a csv file and has 8 columns
+@app.callback(Output('output-data-upload', 'children'),
+              Output('file-path', 'children'),
+              Input('upload-data', 'contents'),
+              State('upload-data', 'filename'))
+def upload_file(contents, filename):
+    if contents is None:
+        raise PreventUpdate
+
+    content_type, content_string = contents.split(",")
+    decoded = base64.b64decode(content_string)
+
+    filename = secure_filename(filename)
+    file_path = os.path.join(UPLOAD_FOLDER, filename)
+
+    with open(file_path, 'wb') as f:
+        f.write(decoded)
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        csv_reader = csv.reader(file)
+        for row in csv_reader:
             if len(row) != 8:
-                raise ValidationError("CSV must have 8 columns")
+                os.remove(file_path)
+                return html.Div([
+                    html.H5('CSV must have 8 columns')
+                ]), None
 
+    return html.Div([
+        html.H5('File uploaded successfully')
+    ]), file_path
 
-@app.route('/', methods=['GET', 'POST'])
-def upload():
-    form = UploadForm()
-
-    if form.validate_on_submit():
-        f = form.file.data
-        filename = secure_filename(f.filename)
-        file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-        f.save(file_path)
-
-        osm_data = process_csv_file(file_path)
-        osm_file_path = os.path.join(app.config["DOWNLOAD_FOLDER"], "osm_data.geojson")
+# run the transformation on the uploaded csv file and query the osm api
+@app.callback(Output('output-osm-query', 'children'),
+              Output('osm-file-path', 'children'),
+              Input('output-data-upload', 'children'),
+              State('file-path', 'children'))
+def run_osm_query(upload_status, file_path):
+    if upload_status is None:
+        raise PreventUpdate
+    try:
+        with open(file_path, 'r') as file:
+            osm_data = process_csv_file(file)
+        osm_file_path = os.path.join(DOWNLOAD_FOLDER, "osm_data.geojson")
         with open(osm_file_path, "w") as osm_file:
             geojson.dump(osm_data, osm_file)
+        return html.Div([
+            html.H5('OSM query run successfully')
+        ]), osm_file_path
+    except Exception as e:
+        os.remove(file_path)
+        return html.Div([
+            html.H5('OSM query failed')
+        ]), None
 
+# now lets add the points out of the csv file which is uploaded into the file list.
+# the points are as Easting (m),Northing (m) in the csv file.
+# they are in epsg 31468 and need to be converted to 4326 first for the map.
+# add the points to the map as a layer and add a marker to each point. but group them in a layer group.
+# add a layer control to the map to switch the points on and off.
+@app.callback(Output('points', 'children'),
+              Input('output-data-upload', 'children'),
+              State('file-path', 'children'))
+def show_points(upload_status, file_path):
+    if upload_status is None:
+        raise PreventUpdate
+    df = transfrom_csv(file_path, 31468, 4326)
     
-    return render_template('index.html', form=form)
+    if len(df) > 200:
+        df = df.sample(n=200, random_state=42)  # Select 200 random points
+    
+    points = []
+    for index, row in df.iterrows():
+        points.append(dl.Marker(position=[row['Latitude'], row['Longitude']]))
+    
+    print(points)
+    
+    group = dl.LayerGroup(children=points)
+    
+    if len(df) > 200:
+        return html.Div([
+            html.H6('Showing 200 random points out of {}'.format(len(df))),
+            group
+        ])
+    else:
+        return group
+    
+# TODO Make a display of the classes in the csv with polygons for each class where it is the highest compared to the other classes
+# I want to have polygons for each class in the csv. The edges of the polygons are defined by the points of the class.
+# The polygons should be displayed on the map and be grouped in a layer group for each class.
+# The layer groups should be added to the layer control of the map.
+# The polygons should be colored in different colors.
+# The polygons should be transparent.
+# The polygons should be labeled with the class name.
+# The polygons should be clickable and show the class name.
 
 
-# @app.route("/", methods=["GET", "POST"])
-# def index():
-#     form = UploadForm()
-#     if form.validate_on_submit():
-#         filename = secure_filename(form.file.data.filename)
-#         file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-#         form.file.data.save(file_path)
-#         # file.save(file_path)
+@app.callback(Output('classes', 'children'),
+              Input('output-data-upload', 'children'),
+              State('file-path', 'children'))
+def show_classes(upload_status, file_path):
+    if upload_status is None:
+        raise PreventUpdate
 
-#         osm_data = process_csv_file(file_path)
-#         osm_file_path = os.path.join(app.config["DOWNLOAD_FOLDER"], "osm_data.geojson")
-#         with open(osm_file_path, "w") as osm_file:
-#             geojson.dump(osm_data, osm_file)
+    df = transfrom_csv(file_path, 31468, 4326)
 
-#         # Provide a download link for the transformed OSM data
-#         download_link = url_for("download_file", filename="osm_data.geojson")
+    class_columns = [col for col in df.columns if col.startswith('Class')]
+    df['highest_class'] = df[class_columns].apply(lambda row: row.idxmax() if row.max() > 0 else 'NoClass', axis=1)
+    df.drop(class_columns, axis=1, inplace=True)
 
-#         return render_template("index.html", form=form, download_link=download_link)
-#     return render_template("index.html", form=form)
+    geometry = [
+        shapely.geometry.Point(row['Longitude'], row['Latitude'])
+        if not pd.isna(row['Latitude']) and not pd.isna(row['Longitude'])
+        else None
+        for _, row in df.iterrows()
+    ]
+    gdf = gpd.GeoDataFrame(df, geometry=geometry, crs='EPSG:4326')
+    gdf.drop(['Latitude', 'Longitude'], axis=1, inplace=True)
+    gdf = gdf.dissolve(by='highest_class', aggfunc='sum')
+    gdf.reset_index(inplace=True)
+
+    classes = []
+    for index, row in gdf.iterrows():
+        classes.append(dl.GeoJSON(data=json.dumps(row.geometry.__geo_interface__)))
+
+    return classes
+
+# not used for now
+# @app.callback(Output("markers", "children"), Input("btn", "n_clicks"))
+# def generate_markers(_):
+#     return [dl.Marker(position=[51 + random.random(), 11 + random.random()]) for i in range(5)]
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run_server(debug=True)
