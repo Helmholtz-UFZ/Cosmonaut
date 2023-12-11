7c2e3bff3234729565c05a8592fee4ece31978bd
Louis Ferdinand Trinkle <louis-ferdinand.trinkle@ufz.de>
2023-12-11 17:40:24 +0100

    Update .gitignore

--- transformation.py
+++ transformation.py
@@ -3,18 +3,8 @@ This script reads a csv file with coordinates in EPSG:31468 projection.
 Then converts them to EPSG:4326 projection and calculates the bounding box of the projected csv file.
 Out of this it creates a polygon from the bounding box, buffers the polygon, and queries the Overpass API to get the road network data of OSM inside the buffered polygon.
 """
-import pandas as pd
-from pyproj import CRS, Transformer
-import pyproj
-import numpy as np
-from shapely.ops import cascaded_union
-from shapely.geometry import Point, Polygon, LineString
-
-# import geojson
 import overpass
 import time
-
-
 import pandas as pd
 import numpy as np
 import pyproj
@@ -22,10 +12,77 @@ import overpass
 import time
 from shapely.geometry import Polygon
 from pyproj import CRS, Transformer
+import osmnx as ox
+import osmnx as ox
 
 # TODO use classes instead of functions
 # TODO make functions for each step of the process
 
+class CsvFile:
+    def __init__(self, file_path, epsg_input, epsg_output):
+        self.file_path = file_path
+        self.epsg_input = epsg_input
+        self.epsg_output = epsg_output
+        self.df = self._transform_csv(epsg_input, epsg_output)
+
+    def _transform_csv(self, crs_input, crs_output):
+        """
+        This function takes a CSV file as input, processes it and returns the result of a query to the Overpass API.
+
+        Args:
+            crs_input (str): The input CRS.
+            crs_output (str): The output CRS.
+
+        Returns:
+            dict: A dictionary containing the result of the query to the Overpass API.
+
+        """
+        # crs_output = CRS.from_string(crs_output)
+        # crs_input = CRS.from_string(crs_input)
+        transformer = Transformer.from_crs(crs_input, crs_output)
+
+        df = pd.read_csv(self.file_path)
+
+        df["Latitude"], df["Longitude"] = transformer.transform(
+            df["Easting (m)"].values, df["Northing (m)"].values
+        )
+
+        df.drop(
+            ["Easting (m)", "Northing (m)"], axis=1, inplace=True
+        )
+
+        return df
+
+    def get_convex_hull(self):
+        points = self.df["Longitude", "Latitude"]
+        hull = points.convex_hull
+        hull_buffer = hull.buffer(0.005, cap_style="square", join_style=2)
+
+        return hull_buffer
+        
+class OverpassQuery:
+    def __init__(self, polygon):
+        self.polygon = polygon
+        self.query = self.get_query()
+        self.result = self.get_result()
+    
+    # Query the Overpass API with the polygon and osmnx (features_from_polygon) amd make it so the user can choose the type of road network to download
+    def get_query(self):
+        ox.config(use_cache=True, log_console=True)
+        tags = {
+            'highway': ['primary', 'secondary', 'tertiary', 'unclassified', 'residential', 'primary_link', 'secondary_link', 'tertiary_link', 'living_street', 'track', 'road']
+        }
+        osm_data = ox.geometries_from_polygon(self.polygon, tags=tags)
+        return osm_data
+    
+    # Transform OSM data to csv EPSG
+    def osm_transform(self):
+        # Define EPSG.
+        epsg_input = 4326
+        epsg_output = 31468
+        osm_data = self.osm_data
+
+        
 def transfrom_csv(input_file, epsg_input, epsg_output):
     """
     This function takes a CSV file as input, processes it and returns the result of a query to the Overpass API.
