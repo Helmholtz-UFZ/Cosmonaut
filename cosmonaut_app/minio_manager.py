caff6dd51e9d64da1cf4f78f2273b821a91057ef
Louis Ferdinand Trinkle <louis-ferdinand.trinkle@ufz.de>
2024-03-21 16:38:11 +0100

    Add simple ENV add

--- cosmonaut_app/minio_manager.py
+++ cosmonaut_app/minio_manager.py
@@ -1,11 +1,20 @@
 from minio import Minio
 
+# TODO: Temporary loading of environment variables, later should be automatically loaded for Docker
+from dotenv import load_dotenv
+load_dotenv('.env_test_priv')
+
+from config import (
+    MINIO_ACCESS_KEY,
+    MINIO_SECRET_KEY
+)
+
 class MiniIOManager:
     def __init__(self, bucket_name):
         self.bucket_name = bucket_name
         self.minio_client = Minio('minio.ufz.de',
-                                  access_key='MrB4LovDjMtWdegRIyOk',
-                                  secret_key='a0D4Yvvd1VAATEYnGzBgqOqYIfgXmIbt5ZPloxC8',
+                                  access_key=MINIO_ACCESS_KEY,
+                                  secret_key=MINIO_SECRET_KEY,
                                   secure=True)
 
     def upload_file(self, file_path, object_key):
@@ -27,8 +36,8 @@ if __name__ == "__main__":
     manager = MiniIOManager(bucket_name)
     try:
         manager.minio_client.bucket_exists(bucket_name)
-        manager.upload_file("path/to/local/file.txt", "object_key/file.txt")
-        manager.delete_file("object_key/file.txt")
+        manager.upload_file("test_data/no_csv.txt", "no_csv.txt")
+        manager.delete_file("no_csv.txt")
     except Exception as e:
         print(f"Bucket {bucket_name} does not exist: {str(e)}")
         
