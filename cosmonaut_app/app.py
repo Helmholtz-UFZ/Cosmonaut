8ebe8e113022301097e12332d3a8f0bf127a4d6c
Louis Ferdinand Trinkle <louis-ferdinand.trinkle@ufz.de>
2024-05-27 18:34:59 +0200

    Update port

--- cosmonaut_app/app.py
+++ cosmonaut_app/app.py
@@ -1,8 +1,9 @@
 from layout import app
 from flask_routes import server
 import callbacks  # This import is necessary to register the callbacks with the app
+from cosmonaut_app.config import PORT
 
 app.server = server
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(host="0.0.0.0", debug=True, port=PORT)
