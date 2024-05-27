a919042ac0d8b2e6a0b16f0f25c9fd9b516a48b2
Louis Ferdinand Trinkle <louis-ferdinand.trinkle@ufz.de>
2024-05-27 14:56:40 +0200

    __innit__.py

---
+++ cosmonaut_app/__init__.py
@@ -0,0 +1,6 @@
+"""
+Package Initialization (__init__.py).
+
+This module serves as the entry point for the cosmonaut web server application. It
+contains essential initialization code and package-level documentation.
+"""
