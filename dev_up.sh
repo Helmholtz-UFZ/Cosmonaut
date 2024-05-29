1456969e4f09ff2cacbf0bdf2a7ac53adb47d52b
Louis Ferdinand Trinkle <louis-ferdinand.trinkle@ufz.de>
2024-05-29 14:54:52 +0200

    Auto Container rebuild if dependencies changed

--- dev_up.sh
+++ dev_up.sh
@@ -8,7 +8,7 @@ fi
 if [ "$1" == "mock" ]; then
     env_file=".env_test_priv"
 elif [ "$1" == "prod" ]; then
-    env_file=".env_test_priv_123"
+    env_file=".env_prod_priv"
 else
     echo "Usage: $0 <mock|prod>"
     echo "Invalid mode. Use 'mock' or 'prod'."
@@ -21,6 +21,15 @@ if [ ! -e "$env_file" ]; then
 fi
 
 cp "$env_file" .env
+
+# Check if the poetry.lock file has changed since the last Docker build
+if [ ! -e ".docker_build_hash" ] || [ "$(sha256sum poetry.lock)" != "$(cat .docker_build_hash)" ]; then
+    # Build the Docker image
+    docker compose build cosmonaut
+
+    # Save the hash of the poetry.lock file
+    sha256sum poetry.lock > .docker_build_hash
+fi
 
 if [ "$1" == "prod" ]; then
     docker compose up --no-log-prefix cosmonaut
