857cdb8fe3ab3e3d254df190c3825270d2ec6490
Louis Ferdinand Trinkle <louis-ferdinand.trinkle@ufz.de>
2024-06-10 17:10:07 +0200

    Update init.sql

--- docker/init.sql
+++ docker/init.sql
@@ -8,13 +8,17 @@ DROP TABLE IF EXISTS jobs;
 CREATE TABLE jobs (
     job_id VARCHAR PRIMARY KEY,
     start_date DATE,
+    end_date DATE,
     input_data JSONB,
     files BYTEA[],
     file_names VARCHAR[],
     submitted BOOL,
     email VARCHAR,
     notified_end BOOL,
-    logs VARCHAR,
     status VARCHAR,
     version DECIMAL
 );
+
+-- psql -U cosmonaut -p 5432 -h localhost -d cosmonaut_db
+-- psql -U USER -p PORT -h HOST -d DATABASE
+-- SELECT * FROM jobs;
