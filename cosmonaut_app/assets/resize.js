f3a1e99293f3f4c78ed83d7e6086c1e60d9d1e20
Louis Ferdinand Trinkle <louis-ferdinand.trinkle@ufz.de>
2024-08-23 11:21:34 +0200

    Add Resize comment

--- cosmonaut_app/assets/resize.js
+++ cosmonaut_app/assets/resize.js
@@ -1,3 +1,4 @@
+// This is a workaround to resize the map when the sidebar is toggled as Dash doesn't support resizing the map when the sidebar is toggled.
 window.addEventListener('resize', function() {
     var sidebarWidth = document.getElementById('offcanvas').offsetWidth;
     document.getElementById('main-map').style.width = `calc(100% - ${sidebarWidth}px)`;
