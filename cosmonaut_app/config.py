a033d3bd74f690cb8bb6264c3bf59341f93fa3cd
john <john-eric.anders@ufz.de>
2026-02-05 11:52:18 +0100

    vars from values.yaml to env_prod

--- cosmonaut_app/config.py
+++ cosmonaut_app/config.py
@@ -50,15 +50,34 @@ env_vars = [
     "WEB_OUTSIDE_URL",
     "OBJECT_STORAGE_PORT",
     "OBJECT_STORAGE_CONSOLE_PORT",
+    "TILESERVER_URL",
+    "MAINTAINER_EMAIL",
+    "EMAIL_SERVER",
+    "EMAIL_PORT",
+    "EMAIL_USERNAME",
 ]
 
+# Devolopment Debug Mode
+DEBUG = getenv("DEBUG") == "1"
+# Not neeeded for service kept for testing
+DOCKER_UID = getenv("DOCKER_UID")
+DOCKER_GID = getenv("DOCKER_GID")
+GUNICORN = getenv("GUNICORN")
+
+# Web Application Configuration
 WEB_WORK_DIR = os.path.abspath(getenv("WEB_WORK_DIR"))
+JOB_WORK_DIR_TEMPLATE = os.path.join(WEB_WORK_DIR, "{job_id}")
 WEB_OUTSIDE_URL = getenv("WEB_OUTSIDE_URL")
 FLASK_PORT = getenv("FLASK_PORT")
-REDIS_PORT = getenv("REDIS_PORT")
-REDIS_HOST = getenv("REDIS_HOST")
-REDIS_DB = getenv("REDIS_DB")
-REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", "")  # Optional
+MAINTAINER_EMAIL = getenv("MAINTAINER_EMAIL")
+
+# Tile Server URL
+TILESERVER_URL = getenv("TILESERVER_URL")
+
+# Email Configuration
+EMAIL_SERVER = getenv("EMAIL_SERVER")
+EMAIL_PORT = getenv("EMAIL_PORT")
+EMAIL_USERNAME = getenv("EMAIL_USERNAME")
 
 # Object Storage Configuration (rclone-based S3)
 OBJECT_STORAGE_ACCESS_KEY = getenv("OBJECT_STORAGE_ACCESS_KEY")
@@ -66,19 +85,22 @@ OBJECT_STORAGE_SECRET_KEY = getenv("OBJECT_STORAGE_SECRET_KEY")
 OBJECT_STORAGE_HOST = getenv("OBJECT_STORAGE_HOST")
 OBJECT_STORAGE_BUCKET = getenv("OBJECT_STORAGE_BUCKET")
 OBJECT_STORAGE_REMOTE_NAME = getenv("OBJECT_STORAGE_REMOTE_NAME")
-JOB_WORK_DIR_TEMPLATE = os.path.join(WEB_WORK_DIR, "{job_id}")
 # Not neeeded for service kept for testing
 OBJECT_STORAGE_PORT = getenv("OBJECT_STORAGE_PORT")
 OBJECT_STORAGE_CONSOLE_PORT = getenv("OBJECT_STORAGE_CONSOLE_PORT")
 
+# PostgreSQL Configuration
 POSTGRES_NAME = getenv("POSTGRES_NAME")
 POSTGRES_HOST_NAME = getenv("POSTGRES_HOST_NAME")
 POSTGRES_PORT = getenv("POSTGRES_PORT")
 POSTGRES_USER = getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = getenv("POSTGRES_PASSWORD")
-DOCKER_UID = getenv("DOCKER_UID")
-DOCKER_GID = getenv("DOCKER_GID")
-DEBUG = getenv("DEBUG") == "1"
+
+# Redis Configuration
+REDIS_PORT = getenv("REDIS_PORT")
+REDIS_HOST = getenv("REDIS_HOST")
+REDIS_DB = getenv("REDIS_DB")
+REDIS_PASSWORD = getenv("REDIS_PASSWORD")
 
 # Mapping of OSM tags to the corresponding road classes
 osm_tags_mapping = {
